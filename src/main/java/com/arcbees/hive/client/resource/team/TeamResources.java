95147ca391e40b32d1e5597f86b73c433de91469
Simon-Pierre Gingras <simon.pierre.gingras@arcbees.com>
2012-06-12 17:51:19 -0400

    Team members pictures

---
+++ src/main/java/com/arcbees/hive/client/resource/team/TeamResources.java
@@ -0,0 +1,30 @@
+package com.arcbees.hive.client.resource.team;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+public interface TeamResources extends ClientBundle {
+    ImageResource team_chris_goudreau();
+
+    ImageResource team_chris_viel();
+
+    ImageResource team_david_marcotte();
+
+    ImageResource team_francis_menard();
+
+    ImageResource team_philippe_beaudoin();
+
+    ImageResource team_simon_pierre_gingras();
+
+    ImageResource team_sylvain_perron();
+
+    ImageResource team_vincent_roseberry();
+
+    ImageResource team_brandon_donnelson();
+
+    ImageResource team_javier_fernandez();
+
+    ImageResource team_maxime_meriouma();
+
+    ImageResource team_frank_poulin();
+}

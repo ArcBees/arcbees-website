469a4a24afc242cf0acfb238f1130c6bd12390e7
albre <bretn.alexandr@gmail.com>
2014-05-06 15:47:50 -0400

    Fix all pages but carousel

--- src/main/java/com/arcbees/hive/client/resource/ResourcesLoader.java
+++ src/main/java/com/arcbees/hive/client/resource/ResourcesLoader.java
@@ -2,13 +2,29 @@ package com.arcbees.hive.client.resource;
 
 import javax.inject.Inject;
 
+import com.arcbees.hive.client.application.common.CustomersPresenter;
+import com.arcbees.hive.client.resource.customers.CustomersResources;
+import com.arcbees.hive.client.resource.home.HomeResources;
+import com.arcbees.hive.client.resource.products.ProductsResources;
+import com.arcbees.hive.client.resource.reinit.ReinitResources;
+import com.arcbees.hive.client.resource.socialmedia.SocialMediaResources;
 import com.arcbees.hive.client.resource.team.TeamResources;
 
 public class ResourcesLoader {
     @Inject
     ResourcesLoader(Resources resources,
-                    TeamResources teamResources) {
+                    ReinitResources reinitResources,
+                    TeamResources teamResources,
+                    SocialMediaResources socialMediaResources,
+                    ProductsResources productsResources,
+                    HomeResources homeResources,
+                    CustomersResources customersResources) {
+        reinitResources.style().ensureInjected();
         resources.style().ensureInjected();
         teamResources.style().ensureInjected();
+        socialMediaResources.style().ensureInjected();
+        productsResources.style().ensureInjected();
+        homeResources.style().ensureInjected();
+        customersResources.style().ensureInjected();
     }
 }

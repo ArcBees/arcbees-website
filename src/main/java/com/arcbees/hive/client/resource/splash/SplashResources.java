65291ce0519e61d5aebda50e29718fc8dd324ba7
albre <bretn.alexandr@gmail.com>
2014-06-12 16:10:51 -0400

    Remove fonts

--- src/main/java/com/arcbees/hive/client/resource/splash/SplashResources.java
+++ src/main/java/com/arcbees/hive/client/resource/splash/SplashResources.java
@@ -18,7 +18,6 @@ package com.arcbees.hive.client.resource.splash;
 
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.GssResource;
-import com.google.gwt.resources.client.DataResource;
 import com.google.gwt.resources.client.ImageResource;
 
 public interface SplashResources extends ClientBundle {
@@ -50,20 +49,6 @@ public interface SplashResources extends ClientBundle {
         String right();
 
         String socialContainer();
-
-        String typo();
-
-        String boldFam();
-
-        String regularFamBold();
-
-        String regularFam();
-
-        String regularFamB();
-
-        String regularFamL();
-
-        String regularFamO();
     }
 
     Style style();
@@ -72,39 +57,4 @@ public interface SplashResources extends ClientBundle {
 
     ImageResource smallArcbeesLogo();
 
-    @Source("fonts/geometria_light.eot")
-    DataResource geometriaLightEot();
-
-    @Source("fonts/geometria_light.svg")
-    DataResource geometriaLightSvg();
-
-    @Source("fonts/geometria_light.ttf")
-    DataResource geometriaLightTtf();
-
-    @Source("fonts/geometria_light.woff")
-    DataResource geometriaLightWoff();
-
-    @Source("fonts/geometria_regular.eot")
-    DataResource geometriaRegularEot();
-
-    @Source("fonts/geometria_regular.svg")
-    DataResource geometriaRegularSvg();
-
-    @Source("fonts/geometria_regular.ttf")
-    DataResource geometriaRegularTtf();
-
-    @Source("fonts/geometria_regular.woff")
-    DataResource geometriaRegularWoff();
-
-    @Source("fonts/geometria_bold.eot")
-    DataResource geometriaBoldEot();
-
-    @Source("fonts/geometria_bold.svg")
-    DataResource geometriaBoldSvg();
-
-    @Source("fonts/geometria_bold.ttf")
-    DataResource geometriaBoldTtf();
-
-    @Source("fonts/geometria_bold.woff")
-    DataResource geometriaBoldWoff();
 }

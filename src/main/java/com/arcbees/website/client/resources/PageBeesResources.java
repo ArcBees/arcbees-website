1c2c3cd73e09fbe6d737acb01ba023ef20319aa5
jasonlemay <jason.lemay@arcbees.com>
2014-09-04 14:43:01 -0400

    Bees page

---
+++ src/main/java/com/arcbees/website/client/resources/PageBeesResources.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2014 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.arcbees.website.client.resources;
+
+import com.google.gwt.resources.client.GssResource;
+import com.google.gwt.resources.client.ImageResource;
+
+public interface PageBeesResources extends FontResources {
+    interface Style extends GssResource {
+        String bees();
+
+        String slide();
+
+        String slides();
+
+        String prev();
+
+        String next();
+
+        String pager();
+
+        String active();
+
+        String bee();
+
+        String beeHolder1();
+
+        String beeHolder2();
+
+        String bee1();
+
+        String bee2();
+
+        String bee3();
+
+        String hive();
+
+        String intro();
+
+        String icon1();
+
+        String icon2();
+
+        String icon3();
+
+        String icon4();
+
+        String icon5();
+
+        String icon6();
+    }
+
+    @Source("img/bees/christian.jpg")
+    ImageResource beeChristian();
+
+    @Source({"com/arcbees/gsss/mixin/client/mixins.gss", "css/colors.gss", "fonts/geometria/geometria.gss", "css/pages/bees.gss"})
+    Style style();
+}

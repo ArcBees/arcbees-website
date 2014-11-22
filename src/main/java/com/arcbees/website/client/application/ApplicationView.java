02eaaf3960d3061a26b89f6e4d8499514574bf00
spgingras <simon.pierre.gingras@arcbees.com>
2014-11-22 11:23:52 -0500

    i18n POC

--- src/main/java/com/arcbees/website/client/application/ApplicationView.java
+++ src/main/java/com/arcbees/website/client/application/ApplicationView.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2014 ArcBees Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -20,7 +20,9 @@ import com.arcbees.website.client.resources.AppResources;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.i18n.client.LocaleInfo;
 import com.google.gwt.query.client.Function;
+import com.google.gwt.query.client.GQuery;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Event;
@@ -46,6 +48,8 @@ public class ApplicationView extends ViewImpl implements ApplicationPresenter.My
     DivElement menuToggle;
     @UiField
     DivElement content;
+    @UiField
+    DivElement langToggle;
 
     private final AppResources appResources;
 
@@ -97,6 +101,29 @@ public class ApplicationView extends ViewImpl implements ApplicationPresenter.My
                 removeActiveStyle();
             }
         });
+
+        $(langToggle).click(new Function() {
+            @Override
+            public void f() {
+                switchLang();
+            }
+        });
+    }
+
+    private void switchLang() {
+        LocaleInfo currentLocale = LocaleInfo.getCurrentLocale();
+
+        String newLocale;
+
+        if (currentLocale.getLocaleName().equals("fr")) {
+            newLocale = "en";
+        } else {
+            newLocale = "fr";
+        }
+
+        Window.Location.assign(Window.Location.createUrlBuilder()
+                .setParameter(LocaleInfo.getLocaleQueryParam(), newLocale)
+                .buildString());
     }
 
     private void removeActiveStyle() {

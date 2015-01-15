11e3a7b1f8a60bca50adcc8071c2e47288bf3ef2
Christopher Viel <viel.christopher@gmail.com>
2015-01-15 13:42:24 -0500

    Intelligently select the default locale when accessed from root.

--- src/main/java/com/arcbees/website/server/HomeServlet.java
+++ src/main/java/com/arcbees/website/server/HomeServlet.java
@@ -19,8 +19,6 @@ package com.arcbees.website.server;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -30,36 +28,22 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 
-import com.google.common.base.Strings;
 import com.google.common.io.ByteSource;
 import com.google.common.io.Resources;
 
 public class HomeServlet extends HttpServlet {
     private static final String VELOCITY_PROPERTIES = "com/arcbees/website/server/velocity.properties";
     private static final String HOME_TEMPLATE = "com/arcbees/website/server/html/home.vm";
-    private static final String DEFAULT_LOCALE = "en";
-    private static final Pattern EXTRACT_LOCALE_PATTERN = Pattern.compile("^/(en|fr)(?:/.*)?");
 
     private VelocityEngine velocityEngine;
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String locale = new LocaleExtractor(request, response).extractLocale();
         VelocityContext context = new VelocityContext();
-        context.put("locale", extractLocale(request));
+        context.put("locale", locale);
 
         getVelocityEngine().mergeTemplate(HOME_TEMPLATE, "UTF-8", context, response.getWriter());
-    }
-
-    private String extractLocale(HttpServletRequest request) {
-        String path = Strings.nullToEmpty(request.getRequestURI());
-        Matcher matcher = EXTRACT_LOCALE_PATTERN.matcher(path);
-
-        String locale = null;
-        if (matcher.find()) {
-            locale = matcher.group(1);
-        }
-
-        return Strings.isNullOrEmpty(locale) ? DEFAULT_LOCALE : locale;
     }
 
     private VelocityEngine getVelocityEngine() {
